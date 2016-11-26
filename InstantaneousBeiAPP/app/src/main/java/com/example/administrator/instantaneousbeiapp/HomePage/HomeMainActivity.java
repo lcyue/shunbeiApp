b64a51d1bf7ae6a39a14dfe0b2015c414db304bf
lcyue <577025896@qq.com>
2016-11-26 18:34:23 +0800

    完成钱包的所有跳转，和添加分类的跳转

--- InstantaneousBeiAPP/app/src/main/java/com/example/administrator/instantaneousbeiapp/HomePage/HomeMainActivity.java
+++ InstantaneousBeiAPP/app/src/main/java/com/example/administrator/instantaneousbeiapp/HomePage/HomeMainActivity.java
@@ -1,6 +1,8 @@
 package com.example.administrator.instantaneousbeiapp.HomePage;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -8,7 +10,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.example.administrator.instantaneousbeiapp.Detail.DemoCeHua;
@@ -16,6 +23,11 @@ import com.example.administrator.instantaneousbeiapp.HomePage.Fragment.DetailFra
 import com.example.administrator.instantaneousbeiapp.HomePage.Fragment.StatementFragment;
 import com.example.administrator.instantaneousbeiapp.HomePage.Fragment.WalletFragment;
 import com.example.administrator.instantaneousbeiapp.R;
+import com.example.administrator.instantaneousbeiapp.Wallet.WalletChangeActivity;
+import com.example.administrator.instantaneousbeiapp.jizhang.ShouRu;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 
@@ -26,10 +38,17 @@ public class HomeMainActivity extends FragmentActivity {
     TextView iconDetailText;
     TextView iconWalleText;
     TextView iconStatisticalText;
-    TextView icon_more_text;
+    TextView iconMoretext;
+    ImageView typeSelectButton;
     DemoCeHua menu;
     ViewPager viewPager;
     ArrayList<Fragment> fragmentArrayList;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,9 +58,11 @@ public class HomeMainActivity extends FragmentActivity {
         iconDetailText = (TextView) findViewById(R.id.icon_detail_text);
         iconWalleText = (TextView) findViewById(R.id.icon_wallet_text);
         iconStatisticalText = (TextView) findViewById(R.id.icon_statistical_text);
-        icon_more_text = (TextView) findViewById(R.id.icon_more_text);
-        menu = (DemoCeHua)findViewById(R.id.menu);
-Log.i("HomeMainActivity",""+this.getTaskId());
+        iconMoretext = (TextView) findViewById(R.id.icon_more_text);
+        menu = (DemoCeHua) findViewById(R.id.menu);
+        typeSelectButton = (ImageView) findViewById(R.id.type_select_button);
+
+        Log.i("HomeMainActivity", "" + this.getTaskId());
         fragmentArrayList = new ArrayList<Fragment>();
         DetailFragment detailFragment = new DetailFragment();
         WalletFragment walletFragment = new WalletFragment();
@@ -55,16 +76,19 @@ Log.i("HomeMainActivity",""+this.getTaskId());
         iconDetailText.setOnClickListener(onClickListener);
         iconWalleText.setOnClickListener(onClickListener);
         iconStatisticalText.setOnClickListener(onClickListener);
-        icon_more_text.setOnClickListener(onClickListener);
-
+        iconMoretext.setOnClickListener(onClickListener);
         viewPager.setOnPageChangeListener(onPageChangeListener);
+        typeSelectButton.setOnClickListener(onClickListener);
 
-
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void move(){
+    public void move() {
         menu.togleMenu();
     }
+
 
     //    重写FragmentPagerAdapter方法
     public class InstantaneousPagerAdapter extends FragmentPagerAdapter {
@@ -139,9 +163,11 @@ Log.i("HomeMainActivity",""+this.getTaskId());
         }
     };
 
+    //点击事件
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            Intent intent;
             switch (view.getId()) {
                 case R.id.icon_detail_text:
                     viewPager.setCurrentItem(0);
@@ -151,11 +177,14 @@ Log.i("HomeMainActivity",""+this.getTaskId());
                     iconWalleText.setCompoundDrawablesWithIntrinsicBounds(null, top1, null, null);
                     Drawable top2 = getResources().getDrawable(R.mipmap.icon_statistical_off);
                     iconStatisticalText.setCompoundDrawablesWithIntrinsicBounds(null, top2, null, null);
+                    Drawable top15 = getResources().getDrawable(R.mipmap.icon_more_off);
+                    iconMoretext.setCompoundDrawablesWithIntrinsicBounds(null, top15, null, null);
 
 
                     iconDetailText.setTextColor(getResources().getColor(R.color.green_btn));
                     iconWalleText.setTextColor(getResources().getColor(R.color.gery_btn));
                     iconStatisticalText.setTextColor(getResources().getColor(R.color.gery_btn));
+                    iconMoretext.setTextColor(getResources().getColor(R.color.gery_btn));
                     break;
                 case R.id.icon_wallet_text:
                     viewPager.setCurrentItem(1);
@@ -165,10 +194,13 @@ Log.i("HomeMainActivity",""+this.getTaskId());
                     iconDetailText.setCompoundDrawablesWithIntrinsicBounds(null, top4, null, null);
                     Drawable top5 = getResources().getDrawable(R.mipmap.icon_statistical_off);
                     iconStatisticalText.setCompoundDrawablesWithIntrinsicBounds(null, top5, null, null);
+                    Drawable top14 = getResources().getDrawable(R.mipmap.icon_more_off);
+                    iconMoretext.setCompoundDrawablesWithIntrinsicBounds(null, top14, null, null);
 
                     iconWalleText.setTextColor(getResources().getColor(R.color.green_btn));
                     iconDetailText.setTextColor(getResources().getColor(R.color.gery_btn));
                     iconStatisticalText.setTextColor(getResources().getColor(R.color.gery_btn));
+                    iconMoretext.setTextColor(getResources().getColor(R.color.gery_btn));
                     break;
                 case R.id.icon_statistical_text:
                     viewPager.setCurrentItem(2);
@@ -178,15 +210,75 @@ Log.i("HomeMainActivity",""+this.getTaskId());
                     iconDetailText.setCompoundDrawablesWithIntrinsicBounds(null, top7, null, null);
                     Drawable top8 = getResources().getDrawable(R.mipmap.icon_wallet_off);
                     iconWalleText.setCompoundDrawablesWithIntrinsicBounds(null, top8, null, null);
+                    Drawable top13 = getResources().getDrawable(R.mipmap.icon_more_off);
+                    iconMoretext.setCompoundDrawablesWithIntrinsicBounds(null, top13, null, null);
 
                     iconStatisticalText.setTextColor(getResources().getColor(R.color.green_btn));
                     iconDetailText.setTextColor(getResources().getColor(R.color.gery_btn));
                     iconWalleText.setTextColor(getResources().getColor(R.color.gery_btn));
+                    iconMoretext.setTextColor(getResources().getColor(R.color.gery_btn));
                     break;
                 case R.id.icon_more_text:
-                    move();
+                    viewPager.setCurrentItem(3);
+                    Drawable top9 = getResources().getDrawable(R.mipmap.icon_detail_off);
+                    iconDetailText.setCompoundDrawablesWithIntrinsicBounds(null, top9, null, null);
+                    Drawable top10 = getResources().getDrawable(R.mipmap.icon_wallet_off);
+                    iconWalleText.setCompoundDrawablesWithIntrinsicBounds(null, top10, null, null);
+                    Drawable top11 = getResources().getDrawable(R.mipmap.icon_statistical_off);
+                    iconStatisticalText.setCompoundDrawablesWithIntrinsicBounds(null, top11, null, null);
+                    Drawable top16 = getResources().getDrawable(R.mipmap.icon_more_on);
+                    iconMoretext.setCompoundDrawablesWithIntrinsicBounds(null, top16, null, null);
+
+                    iconDetailText.setTextColor(getResources().getColor(R.color.gery_btn));
+                    iconWalleText.setTextColor(getResources().getColor(R.color.gery_btn));
+                    iconStatisticalText.setTextColor(getResources().getColor(R.color.gery_btn));
+                    iconMoretext.setTextColor(getResources().getColor(R.color.green_btn));
                     break;
+                case R.id.type_select_button:
+                        if (bool == true) {
+                            popupWindow.dismiss();
+                            bool = popupWindow.isShowing();
+                        } else {
+                            createPopupWindow();//最后需要动画进行美观 以便形成半圆的效果
+                            bool = popupWindow.isShowing();
+                        }
+                    break;
+                case R.id.dudget_type:
+                    intent = new Intent(HomeMainActivity.this, ShouRu.class);
+                    startActivity(intent);
+                    break;
+                case R.id.account_type:
+                    intent = new Intent(HomeMainActivity.this, WalletChangeActivity.class);
+                    startActivity(intent);
+                    break;
+
             }
         }
     };
+
+    //选择类型的页面
+    boolean bool;
+    PopupWindow popupWindow;
+    Button dudgetType;
+    Button accountType;
+    View view;
+    LayoutInflater layoutInflater;
+    public void createPopupWindow() {
+        layoutInflater = LayoutInflater.from(HomeMainActivity.this);
+        view = layoutInflater.inflate(R.layout.type_select_button_layout, null);
+        popupWindow = new PopupWindow(view);
+        popupWindow.setWidth(400);
+        popupWindow.setHeight(100);
+        popupWindow.setOutsideTouchable(true);
+        bool = popupWindow.isShowing();
+        popupWindow.showAtLocation(view, Gravity.CENTER | Gravity.BOTTOM, 0, 400);
+
+        dudgetType = (Button) view.findViewById(R.id.dudget_type);
+        accountType = (Button)view.findViewById(R.id.account_type);
+
+        dudgetType.setOnClickListener(onClickListener);
+        accountType.setOnClickListener(onClickListener);
+
+
+    }
 }
