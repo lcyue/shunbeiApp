cf43a6b220aa2221ab4fd186681bc1bc323a642c
puzuhui <2753015604@qq.com>
2016-11-27 16:05:36 +0800

    记账的修改

--- InstantaneousBeiAPP/app/src/main/java/com/example/administrator/instantaneousbeiapp/jizhang/XuanzheShijianActivity.java
+++ InstantaneousBeiAPP/app/src/main/java/com/example/administrator/instantaneousbeiapp/jizhang/XuanzheShijianActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
@@ -25,11 +26,25 @@ public class XuanzheShijianActivity extends Activity implements AdapterView.OnIt
     TextView jizhang_zhichu_btn;
     TextView jizhang_xuanzhezhanghu_btn;
     TextView jizhang_time_btn;
+    TextView jizhang_money_btn;
     ImageView jizhang_mine_btn;
     ImageView jizhang_bianji_btn;
     GridView gridView;
     ImageView jizhang_xuanzhe_image;
     TextView jizhang_xuanzhe_text;
+    LinearLayout jisuanqi;        //计算器
+    LinearLayout shunbei_beizhu;  //输入备注
+    LinearLayout jizhang_xuanzhezhanghu;        //选择账户
+    LinearLayout xuanzheshijian;  //选择时间
+    LinearLayout xuanzhechengyuan;  //选择成员
+    ImageView jizhang_gou;
+    ImageView jizhang_gou1;
+    ImageView jizhang_gou2;
+    ImageView jizhang_gou3;
+    LinearLayout one;
+    LinearLayout two;
+    LinearLayout three;
+    LinearLayout fore;
     List<HashMap<String,Object>> list;
     int[] image ={R.mipmap.salary,R.mipmap.job,R.mipmap.red_packet,R.mipmap.alimony,R.mipmap.pin,R.mipmap.investment,
                   R.mipmap.bonus, R.mipmap.apply,R.mipmap.cash,R.mipmap.refund,R.mipmap.alipay,R.mipmap.rest,R.mipmap.compile};
@@ -50,6 +65,20 @@ public class XuanzheShijianActivity extends Activity implements AdapterView.OnIt
         jizhang_xuanzhe_image = (ImageView) findViewById(R.id.jizhang_xuanzhe_image);
         jizhang_xuanzhe_text = (TextView) findViewById(R.id.jizhang_xuanzhe_text);
         gridView= (GridView) findViewById(R.id.gridview);
+        shunbei_beizhu = (LinearLayout) findViewById(R.id.shunbei_beizhu);
+        jisuanqi = (LinearLayout) findViewById(R.id.jisuanqi);
+        jizhang_xuanzhezhanghu = (LinearLayout) findViewById(R.id.jizhang_xuanzhezhanghu);
+        xuanzheshijian = (LinearLayout) findViewById(R.id.xuanzheshijian);
+        xuanzhechengyuan = (LinearLayout) findViewById(R.id.xuanzhechengyuan);
+        jizhang_money_btn = (TextView) findViewById(R.id.jizhang_money_btn);
+        jizhang_gou= (ImageView) findViewById(R.id.jizhang_gou);
+        jizhang_gou1= (ImageView) findViewById(R.id.jizhang_gou1);
+        jizhang_gou2= (ImageView) findViewById(R.id.jizhang_gou2);
+        jizhang_gou3= (ImageView) findViewById(R.id.jizhang_gou3);
+        one = (LinearLayout) findViewById(R.id.one);
+        two = (LinearLayout) findViewById(R.id.two);
+        three = (LinearLayout) findViewById(R.id.three);
+        fore = (LinearLayout) findViewById(R.id.fore);
 
         /**
          * 简单适配器
@@ -68,7 +97,11 @@ public class XuanzheShijianActivity extends Activity implements AdapterView.OnIt
         jizhang_xuanzhezhanghu_btn.setOnClickListener(onClickListener);
         jizhang_shouru_btn.setOnClickListener(onClickListener);
         jizhang_zhichu_btn.setOnClickListener(onClickListener);
-
+        jizhang_money_btn.setOnClickListener(onClickListener);
+        one.setOnClickListener(onClickListener);
+        two.setOnClickListener(onClickListener);
+        three.setOnClickListener(onClickListener);
+        fore.setOnClickListener(onClickListener);
     }
 
     //数据源
@@ -89,28 +122,97 @@ public class XuanzheShijianActivity extends Activity implements AdapterView.OnIt
         public void onClick(View v) {
             Intent intent;
             switch (v.getId()){
+                //点击备注
                 case R.id.jizhang_bianji_btn:
-                    intent = new Intent(XuanzheShijianActivity.this,TianjiaoshouruActivity.class);
-                    startActivity(intent);
+                     if(shunbei_beizhu.getVisibility()==View.GONE){
+                         shunbei_beizhu.setVisibility(View.VISIBLE);
+                         jisuanqi.setVisibility(View.GONE);
+                         jizhang_xuanzhezhanghu.setVisibility(View.GONE);
+                         xuanzheshijian.setVisibility(View.GONE);
+                         xuanzhechengyuan.setVisibility(View.GONE);
+                     }
                     break;
+                //点击选择账户
                 case R.id.jizhang_xuanzhezhanghu_btn:
-                    intent = new Intent(XuanzheShijianActivity.this,XuanzheZhanghuActivity.class);
-                    startActivity(intent);
+                    if(jizhang_xuanzhezhanghu.getVisibility()==View.GONE){
+                        shunbei_beizhu.setVisibility(View.GONE);
+                        jisuanqi.setVisibility(View.GONE);
+                        jizhang_xuanzhezhanghu.setVisibility(View.VISIBLE);
+                        xuanzheshijian.setVisibility(View.GONE);
+                        xuanzhechengyuan.setVisibility(View.GONE);
+                    }
+
                     break;
+                //点击选择时间
                 case R.id.jizhang_time_btn:
-                    intent = new Intent(XuanzheShijianActivity.this,ShouRu.class);
-                    startActivity(intent);
+                    if(xuanzheshijian.getVisibility()==View.GONE){
+                        shunbei_beizhu.setVisibility(View.GONE);
+                        jisuanqi.setVisibility(View.GONE);
+                        jizhang_xuanzhezhanghu.setVisibility(View.GONE);
+                        xuanzheshijian.setVisibility(View.VISIBLE);
+                        xuanzhechengyuan.setVisibility(View.GONE);
+                    }
                     break;
+                //点击选择成员
                 case R.id.jizhang_mine_btn:
-                    intent = new Intent(XuanzheShijianActivity.this,XuanzheChengyuanActivity.class);
-                    startActivity(intent);
+                    if(xuanzhechengyuan.getVisibility()==View.GONE){
+                        shunbei_beizhu.setVisibility(View.GONE);
+                        jisuanqi.setVisibility(View.GONE);
+                        jizhang_xuanzhezhanghu.setVisibility(View.GONE);
+                        xuanzheshijian.setVisibility(View.GONE);
+                        xuanzhechengyuan.setVisibility(View.VISIBLE);
+                    }
                     break;
+                //点击支出
                 case R.id.jizhang_zhichu_btn:
                     intent = new Intent(XuanzheShijianActivity.this,ZhiChuActivity.class);
                     startActivity(intent);
                     break;
+                //点击返回
                 case R.id.jizhang_back_btn:
                     finish();
+                    break;
+                //点击数字
+                case R.id.jizhang_money_btn:
+                    if(jisuanqi.getVisibility()==View.GONE){
+                        shunbei_beizhu.setVisibility(View.GONE);
+                        jisuanqi.setVisibility(View.VISIBLE);
+                        jizhang_xuanzhezhanghu.setVisibility(View.GONE);
+                        xuanzheshijian.setVisibility(View.GONE);
+                        xuanzhechengyuan.setVisibility(View.GONE);
+                    }
+                    break;
+                case R.id.fore:
+                    if(jizhang_gou3.getVisibility()==View.GONE){
+                        jizhang_gou.setVisibility(View.GONE);
+                        jizhang_gou3.setVisibility(View.VISIBLE);
+                        jizhang_gou2.setVisibility(View.GONE);
+                        jizhang_gou1.setVisibility(View.GONE);
+                    }
+                    break;
+                case R.id.three:
+                    if(jizhang_gou2.getVisibility()==View.GONE){
+                        jizhang_gou.setVisibility(View.GONE);
+                        jizhang_gou2.setVisibility(View.VISIBLE);
+                        jizhang_gou3.setVisibility(View.GONE);
+                        jizhang_gou1.setVisibility(View.GONE);
+                    }
+                    break;
+                case R.id.two:
+                    if(jizhang_gou1.getVisibility()==View.GONE){
+                        jizhang_gou.setVisibility(View.GONE);
+                        jizhang_gou1.setVisibility(View.VISIBLE);
+                        jizhang_gou2.setVisibility(View.GONE);
+                        jizhang_gou3.setVisibility(View.GONE);
+                    }
+                    break;
+                case R.id.one:
+                    if(jizhang_gou.getVisibility()==View.GONE){
+                        jizhang_gou3.setVisibility(View.GONE);
+                        jizhang_gou.setVisibility(View.VISIBLE);
+                        jizhang_gou2.setVisibility(View.GONE);
+                        jizhang_gou1.setVisibility(View.GONE);
+                    }
                     break;
             }
         }
